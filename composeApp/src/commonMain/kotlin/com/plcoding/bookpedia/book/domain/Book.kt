d7c60109bd7cd79950919e111ad737d5ea2289c1
Rachael Hobbs <hobbs.rachael@gmail.com>
2024-11-30 13:02:28 -0500

    add first domain layer

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/domain/Book.kt
@@ -0,0 +1,16 @@
+package com.plcoding.bookpedia.book.domain
+
+data class Book(
+    val id: String,
+    val title: String,
+    val authors: List<String>,
+    val imageUrl: String,
+    val description: String?,
+    val languages: List<String>,
+    val firstPublishedYear: String?,
+    val averageRating: Double?,
+    val ratingCount: Int?,
+    val numPages: Int?,
+    val numEditions: Int
+
+)
