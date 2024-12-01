4b5ba722a721ffa78b1f65a42842268180535d0b
Rachael Hobbs <hobbs.rachael@gmail.com>
2024-12-01 14:05:52 -0500

    add tab bar

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListScreen.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListScreen.kt
@@ -2,21 +2,37 @@ package com.plcoding.bookpedia.book.presentation.book_list
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import cmp_bookpedia.composeapp.generated.resources.Res
+import cmp_bookpedia.composeapp.generated.resources.favorites
+import cmp_bookpedia.composeapp.generated.resources.search_results
 import com.plcoding.bookpedia.book.domain.Book
 import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
 import com.plcoding.bookpedia.core.presentation.DarkBlue
+import com.plcoding.bookpedia.core.presentation.DesertWhite
+import com.plcoding.bookpedia.core.presentation.SandYellow
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun BookListScreenRoot(
@@ -63,6 +79,75 @@ private fun BookListScreen(
             modifier = Modifier
                 .widthIn(max = 400.dp)
                 .fillMaxWidth()
-                .padding(16.dp))
+                .padding(16.dp)
+        )
+
+        Surface(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth(),
+            color = DesertWhite,
+            shape = RoundedCornerShape(
+                topStart = 32.dp,
+                topEnd = 32.dp
+            )
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TabRow(
+                    selectedTabIndex = state.selectedTabIndex,
+                    modifier = Modifier
+                        .padding(vertical = 12.dp)
+                        .widthIn(700.dp)
+                        .fillMaxWidth(),
+                    contentColor = DesertWhite,
+                    containerColor = SandYellow,
+                    indicator = { tabPositions ->
+                        TabRowDefaults.SecondaryIndicator(
+                            color = SandYellow,
+                            modifier = Modifier
+                                .tabIndicatorOffset(tabPositions[state.selectedTabIndex])
+                        )
+                    }
+                ) {
+                    Tab(
+                        selected = state.selectedTabIndex == 0,
+                        onClick = {
+                            onAction(BookListAction.OnTabSelected(0))
+                        },
+                        modifier = Modifier.weight(1f),
+                        selectedContentColor = SandYellow,
+                        unselectedContentColor = Color.Black.copy(alpha = 05f)
+                    ) {
+                        Text(
+                            text = stringResource(Res.string.search_results),
+                            modifier = Modifier
+                                .padding(vertical = 12.dp)
+                        )
+
+                    }
+
+                    Tab(
+                        selected = state.selectedTabIndex == 1,
+                        onClick = {
+                            onAction(BookListAction.OnTabSelected(1))
+                        },
+                        modifier = Modifier.weight(1f),
+                        selectedContentColor = SandYellow,
+                        unselectedContentColor = Color.Black.copy(alpha = 05f)
+                    ) {
+                        Text(
+                            text = stringResource(Res.string.favorites),
+                            modifier = Modifier
+                                .padding(vertical = 12.dp)
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.height(4.dp))
+                // TODO: fix tab bar
+                // TODO: 1:34 -- add the horizontal pager
+            }
+        }
     }
 }
